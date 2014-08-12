b652b01cfc6ab467ac36b4041a172b3fd5dd3050
Bryan Willis <willis.bryan@live.com>
2014-08-12 17:01:26 -0500

    Update sticky-admin-table-headers.php

--- sticky-admin-table-headers.php
+++ sticky-admin-table-headers.php
@@ -12,13 +12,27 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 if ( !function_exists( 'jquery_live_filter_wp_list_tables' ) ) {
-	function jquery_live_filter_wp_list_tables() {
-if( !is_admin() )
-	return;
-function wp_list_tables_sticky_headers_js( $hook_suffix ) {
-    if ( 'plugins.php' !== $hook_suffix || 'edit.php' !== $hook_suffix  )
-        return;
+	function jquery_live_filter_wp_list_tables( $hook_suffix ) {
+	if( !is_admin() )
+		return;
+    	if ( 'plugins.php' !== $hook_suffix || 'edit.php' !== $hook_suffix  )
+        	return;
         wp_enqueue_script( 'jquery-live-filter-wp-list-tables', plugin_dir_url( __FILE__ ) . 'jquery.live-filter-wp-list-tables.js', array( 'jquery' ), '1.0.0', true );	
+        wp_enqueue_script( 'jquery-live-filter-wp-list-tables-init', plugin_dir_url( __FILE__ ) . 'jquery.live-filter-wp-list-tables-init.js', array( 'jquery-live-filter-wp-list-tables' ), '1.0.0', true );	
 }
-	add_action( 'admin_enqueue_scripts', 'wp_list_tables_sticky_headers_js', 20 );
+	add_action( 'admin_enqueue_scripts', 'jquery_live_filter_wp_list_tables', 20 );
+}
+
+
+function jquery_wp_list_tables_filter_css_styles() {
+	$screen = get_current_screen();
+	$base = $screen->base;
+	$name = $screen->id;
+if ( $base === 'edit' || $name == 'plugins' ) { ?>
+	<style type="text/css">
+		.filter-table .quick { margin-left: 0.5em; font-size: 0.8em; text-decoration: none; }
+		.fitler-table .quick:hover { text-decoration: underline; }
+		td.alt { background-color: #ffc; background-color: rgba(255, 255, 0, 0.2); }
+	</style>
+<?php }
 }
